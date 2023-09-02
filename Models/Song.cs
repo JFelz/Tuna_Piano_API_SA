097cd917371554ff9999a6e3e31c815820439d39
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-09-02 18:15:55 -0500

    seeding

---
+++ Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tuna_Piano.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public string Album { get; set;}
+        public int length { get; set; }
+    }
+}
---
+++ Tuna_PianoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tuna_Piano.Models;
+
+namespace Tuna_Piano
+{
+    public class Tuna_PianoDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<SongGenre> SongsGenres { get; set;}
+
+        public Tuna_PianoDbContext(DbContextOptions<Tuna_PianoDbContext> context) : base(context)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData( new Song[]
+            {
+                new Song { Id = 1, Title ="Loyal", ArtistId = 1 ,Album = "A Moment Apart", length = 180},
+                new Song { Id = 1, Title ="", ArtistId = 1 ,Album = "", length = 180},
+                new Song { Id = 1, Title ="", ArtistId = 1 ,Album = "", length = 180},
+            });
+        }
+    }
+}
