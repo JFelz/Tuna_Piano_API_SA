8291f91d23a617046988818aad588b8e382c328c
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-09-03 23:27:04 -0500

    model building

--- Program.cs
+++ Program.cs
@@ -1,13 +1,40 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Tuna_Piano;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost",
+                              "https://localhost")
+                               .AllowAnyHeader()
+                               .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 /*builder.Services.AddNpgsql*/
+builder.Services.AddNpgsql<Tuna_PianoDbContext>(builder.Configuration["TunaPianoDbConnectionString"]);
+
+//JSON Serializer
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 var app = builder.Build();
 
@@ -20,8 +47,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+//APIEndpoints
 
 app.Run();
