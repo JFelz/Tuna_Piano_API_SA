21520dc63b6038f2a06555ac5209c5c5990acd91
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-09-04 22:23:04 -0500

    Finished Artists Starting Genres

---
+++ Models/ArtistSong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Tuna_Piano.Models
+{
+	public class ArtistSong
+	{
+		
+		public int Id { get; set; }
+		[Required]
+		public int ArtistId { get; set; }
+		public Artist Artists { get; set; }
+		public int SongId { get; set; }
+		public Song Songs { get; set; }
+	
+	}
+}
+
